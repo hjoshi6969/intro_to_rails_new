81b1c9cddb6d83d6478ab2c716b234a09b085307
Himanshu <himanshujoshi3304@gmail.com>
2024-03-14 15:58:43 -0500

    initials

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,14 @@
+# app/models/product.rb
 class Product < ApplicationRecord
+  # Associations
   belongs_to :category
   belongs_to :dealer
+  has_many :dealer
+
+  # Validations
+  validates :name, presence: true, length: { maximum: 100 }
+  validates :description, presence: true, length: { maximum: 1000 }
+  validates :price, presence: true, numericality: { greater_than: 0 }
+  validates :category_id, presence: true
+  validates :dealer_id, presence: true
 end
